3924cc8bf1f0dd4f56873a1d6e08b9a0a1ba159d
Dmitry Turubchuk <exe.dmtr@gmail.com>
2022-09-05 20:36:44 +0300

    Create two finc show and hide content

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -2,11 +2,25 @@ const tabs = (headerSelector, tabsSelector, contentsSelector, classActive) => {
     const header = document.querySelector(headerSelector);
     const tabs = document.querySelectorAll(tabsSelector);
     const content = document.querySelectorAll(contentsSelector);
-    const active = document.querySelector(classActive);
-
-
-
     
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = "none";
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(classActive);
+        });
+    };
+
+    function showTabContent(i) {
+        content[i].style.display = "block";
+        tabs[i].classList.add(classActive);
+    };
+   
+
+
 };
 
 export default tabs;
